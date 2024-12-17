450e8c79bd7804554798dd01ae0d8536c690132d
AlexKasema <lexxmulwa@gmail.com>
2024-12-17 15:01:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Travel UI App
+
+- A UI for a travel Application
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div className="text-5xl text-purple-500">HomePage</div>;
+};
+
+export default HomePage;
